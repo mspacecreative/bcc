5ed3e49bc71c6a5594bc8ff3811141bea3634a56
Matt <matt@mspacecreative.com>
2020-03-24 07:35:00 -0300

    c

--- wp-content/themes/bcc/loops/loop-relevant-links.php
+++ wp-content/themes/bcc/loops/loop-relevant-links.php
@@ -8,7 +8,54 @@ $blockanchor = get_field('block_anchor');
 
 if ( $blockanchor ):
 
-if ( $columns == 'two' ):
+if ( $bgcolor == 'lightblue' && $textcolor == 'light' ): ?>
+<div class="section light_blue_bg light">
+<?php elseif ( $bgcolor == 'lightblue' ): ?>
+<div class="section light_blue_bg">
+<?php elseif ( $bgcolor == 'lightgrey' && $textcolor == 'light' ): ?>
+<div class="section light_grey_bg light">
+<?php elseif ( $bgcolor == 'lightgrey' ): ?>
+<div class="section light_grey_bg">
+<?php else : ?>
+<div class="section">
+<?php endif; ?>
+	<div class="inner testing no-top-bottom-padding">
+		
+		<?php if ( $rowheading ) : ?>
+		<div class="inner no-top-bottom-padding centered-title-with-line-rules">
+			<h2 class="bottom-margin-50"><?php echo $rowheading ?></h2>
+		</div>
+		<?php endif; ?>
+		
+		<?php if( have_rows('columns_grid') ): ?>
+		
+		<div class="row between-lg between-md gutter-space-1">
+			
+			<?php while( have_rows('columns_grid') ): the_row(); ?>
+			
+			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">
+				<div class="boxed-content">
+					<?php
+					$heading = get_sub_field('heading');
+					
+					if ( $heading ) {
+						echo '<h3>' . $heading . '</h3>';
+					}
+					
+					include 'includes/cta-button-single.php'; ?>
+				</div>
+			</div>
+			
+			<?php endwhile; ?>
+			
+		</div>
+		
+		<?php endif; ?>
+		
+	</div>
+</div>
+
+<?php elseif ( $columns == 'two' ):
 
 if ( $bgcolor == 'lightblue' && $textcolor == 'light' ): ?>
 <div id="<?php echo $blockanchor ?>" class="section light_blue_bg light">
@@ -166,56 +213,6 @@ if ( $bgcolor == 'lightblue' && $textcolor == 'light' ): ?>
 <div id="<?php echo $blockanchor ?>" class="section">
 <?php endif; ?>
 	<div class="inner no-top-bottom-padding">
-		
-		<?php if ( $rowheading ) : ?>
-		<div class="inner no-top-bottom-padding centered-title-with-line-rules">
-			<h2 class="bottom-margin-50"><?php echo $rowheading ?></h2>
-		</div>
-		<?php endif; ?>
-		
-		<?php if( have_rows('columns_grid') ): ?>
-		
-		<div class="row between-lg between-md gutter-space-1">
-			
-			<?php while( have_rows('columns_grid') ): the_row(); ?>
-			
-			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">
-				<div class="boxed-content">
-					<?php
-					$heading = get_sub_field('heading');
-					
-					if ( $heading ) {
-						echo '<h3>' . $heading . '</h3>';
-					}
-					
-					include 'includes/cta-button-single.php'; ?>
-				</div>
-			</div>
-			
-			<?php endwhile; ?>
-			
-		</div>
-		
-		<?php endif; ?>
-		
-	</div>
-</div>
-
-<?php endif;
-
-else :
-if ( $bgcolor == 'lightblue' && $textcolor == 'light' ): ?>
-<div class="section light_blue_bg light">
-<?php elseif ( $bgcolor == 'lightblue' ): ?>
-<div class="section light_blue_bg">
-<?php elseif ( $bgcolor == 'lightgrey' && $textcolor == 'light' ): ?>
-<div class="section light_grey_bg light">
-<?php elseif ( $bgcolor == 'lightgrey' ): ?>
-<div class="section light_grey_bg">
-<?php else : ?>
-<div class="section">
-<?php endif; ?>
-	<div class="inner testing no-top-bottom-padding">
 		
 		<?php if ( $rowheading ) : ?>
 		<div class="inner no-top-bottom-padding centered-title-with-line-rules">
