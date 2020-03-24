4675e5c243df7975a414a139210ad1389bb2c4ac
Matt <matt@mspacecreative.com>
2020-03-24 18:24:48 -0300

    c

--- wp-content/themes/bcc/assets/js/main.js
+++ wp-content/themes/bcc/assets/js/main.js
@@ -22,32 +22,6 @@
 			$(this).addClass('yt-videos');
 		});
 		
-		var tag = document.createElement('script');
-	    tag.src = "//www.youtube.com/iframe_api";
-	    var firstScriptTag = document.getElementsByTagName('script')[0];
-	    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	
-	    function onYouTubeIframeAPIReady() {
-	        var $ = jQuery;
-	        var players = [];
-	        $('iframe').filter(function(){return this.src.indexOf('http://www.youtube.com/') == 0}).each( function (k, v) {
-	            if (!this.id) { this.id='embeddedvideoiframe' + k }
-	            players.push(new YT.Player(this.id, {
-	                events: {
-	                    'onStateChange': function(event) {
-	                        if (event.data == YT.PlayerState.PLAYING) {
-	                            $.each(players, function(k, v) {
-	                                if (this.getIframe().id != event.target.getIframe().id) {
-	                                    this.pauseVideo();
-	                                }
-	                            });
-	                        }
-	                    }
-	                }
-	            }))
-	        });
-	    }
-		
 		$('.video-mp4').each(function() {
 			$('.video-mp4').click(function() {
 				$(this).addClass('hide-overlay');
