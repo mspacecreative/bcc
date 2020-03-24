584923ffbfd4e7a4a539d3bb0f642498a75ef6a4
Matt <matt@mspacecreative.com>
2020-03-24 07:55:10 -0300

    c

--- wp-content/themes/bcc/loops/loop-relevant-links.php
+++ wp-content/themes/bcc/loops/loop-relevant-links.php
@@ -1,40 +1,45 @@
-<?php 
-$args = array(
-	'post_type' => 'resources',
-	'posts_per_page'=> -1,
-	'tax_query' => array(
-		array(
-		'taxonomy' => 'resource_type',
-		'field' => 'slug',
-		'terms' => 'link',
+<div class="row between-lg between-md gutter-space-1">
+			
+	<?php 
+	$rowheading = get_field('row_heading');
+	if ( $rowheading ) {
+		echo '<h3>' . $rowheading . '</h3>';
+	} ?>
+		
+	<?php 
+	$args = array(
+		'post_type' => 'resources',
+		'posts_per_page'=> -1,
+		'tax_query' => array(
+			array(
+			'taxonomy' => 'resource_type',
+			'field' => 'slug',
+			'terms' => 'link',
+			)
 		)
-	)
-);
-$loop = new WP_Query( $args );
-if ( $loop->have_posts() ) : ?>
-		
-	<div class="row between-lg between-md gutter-space-1">
+	);
+	$loop = new WP_Query( $args );
+	if ( $loop->have_posts() ) : ?>
 			
-		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+	<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 			
-		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">
-			<div class="boxed-content">
-				<?php
-				$rowheading = get_field('row_heading', $post->ID);
-				$content = get_field('content', $post->ID);
+	<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">
+		<div class="boxed-content">
+			<?php
+			$title = get_the_title($post->ID);
+			$content = get_the_content($post->ID);
 				
-					
-				if ( $rowheading ) {
-					echo '<h3>' . $rowheading . '</h3>';
-				}
-				if ( $content ) {
-					echo $content;
-				} ?>
-			</div>
+			if ( $title ) {
+				echo '<h3>' . $title . '</h3>';
+			}
+			if ( $content ) {
+				echo $content;
+			} ?>
 		</div>
-			
-		<?php endwhile; ?>
-			
 	</div>
-		
-<?php endif; wp_reset_query(); ?>
+			
+	<?php endwhile;
+	
+	endif; wp_reset_query(); ?>
+			
+</div>
