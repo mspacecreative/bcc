42ab6edd62ca3b37d9129640da48edd713ffec4e
Matt <matt@mspacecreative.com>
2020-03-24 19:42:07 -0300

    c

--- wp-content/themes/bcc/footer.php
+++ wp-content/themes/bcc/footer.php
@@ -33,42 +33,104 @@
 
 		<!-- analytics -->
 		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
-		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		
-		<script>
-			var tag = document.createElement('script');
-		    tag.src = "https://www.youtube.com/iframe_api";
-		    var firstScriptTag = document.getElementsByTagName('script')[0];
-		    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		
-		    function onYouTubeIframeAPIReady() {
-		        var $ = jQuery;
-		        var players = [];
-		        $('iframe').filter(function(){return this.src.indexOf('https://www.youtube.com/') == 0}).each( function (k, v) {
-		            if (!this.id) { this.id='embeddedvideoiframe' + k }
-		            players.push(new YT.Player(this.id, {
-		                events: {
-		                    'onStateChange': function(event) {
-		                        if (event.data == YT.PlayerState.PLAYING) {
-		                            $.each(players, function(k, v) {
-		                                if (this.getIframe().id != event.target.getIframe().id) {
-		                                    this.pauseVideo();
-		                                }
-		                            });
-		                        }
-		                    }
-		                }
-		            }))
-		        });
-		    }
+		var ytplayerList;
+
+function onPlayerReady(e) {
+    var video_data = e.target.getVideoData(),
+        label = video_data.video_id+':'+video_data.title;
+    e.target.ulabel = label;
+    console.log(label + " is ready!");
+ 
+}
+function onPlayerError(e) {
+    console.log('[onPlayerError]');
+}
+function onPlayerStateChange(e) {
+    var label = e.target.ulabel;
+    if (e["data"] == YT.PlayerState.PLAYING) {
+        console.log({
+            event: "youtube",
+            action: "play:"+e.target.getPlaybackQuality(),
+            label: label
+        });
+        //if one video is play then pause other
+        pauseOthersYoutubes(e.target);
+    }
+    if (e["data"] == YT.PlayerState.PAUSED) {
+        console.log({
+            event: "youtube",
+            action: "pause:"+e.target.getPlaybackQuality(),
+            label: label
+        });
+    }
+    if (e["data"] == YT.PlayerState.ENDED) {
+        console.log({
+            event: "youtube",
+            action: "end",
+            label: label
+        });
+    }
+    //track number of buffering and quality of video
+    if (e["data"] == YT.PlayerState.BUFFERING) {
+        e.target.uBufferingCount?++e.target.uBufferingCount:e.target.uBufferingCount=1; 
+        console.log({
+            event: "youtube",
+            action: "buffering["+e.target.uBufferingCount+"]:"+e.target.getPlaybackQuality(),
+            label: label
+        });
+        //if one video is play then pause other, this is needed because at start video is in buffered state and start playing without go to playing state
+        if( YT.PlayerState.UNSTARTED ==  e.target.uLastPlayerState ){
+            pauseOthersYoutubes(e.target);
+        }
+    }
+    //last action keep stage in uLastPlayerState
+    if( e.data != e.target.uLastPlayerState ) {
+        console.log(label + ":state change from " + e.target.uLastPlayerState + " to " + e.data);
+        e.target.uLastPlayerState = e.data;
+    }
+}
+function initYoutubePlayers(){
+    ytplayerList = null; //reset
+    ytplayerList = []; //create new array to hold youtube player
+    for (var e = document.getElementsByTagName("iframe"), x = e.length; x-- ;) {
+        if (/youtube.com\/embed/.test(e[x].src)) {
+            ytplayerList.push(initYoutubePlayer(e[x]));
+            console.log("create a Youtube player successfully");
+        }
+    }
+    
+}
+function pauseOthersYoutubes( currentPlayer ) {
+    if (!currentPlayer) return;
+    for (var i = ytplayerList.length; i-- ;){
+        if( ytplayerList[i] && (ytplayerList[i] != currentPlayer) ){
+            ytplayerList[i].pauseVideo();
+        }
+    }  
+}
+//init a youtube iframe
+function initYoutubePlayer(ytiframe){
+    console.log("have youtube iframe");
+    var ytp = new YT.Player(ytiframe, {
+        events: {
+            onStateChange: onPlayerStateChange,
+            onError: onPlayerError,
+            onReady: onPlayerReady
+        }
+    });
+    ytiframe.ytp = ytp;
+    return ytp;
+}
+function onYouTubeIframeAPIReady() {
+    console.log("YouTubeIframeAPI is ready");
+    initYoutubePlayers();
+}
+var tag = document.createElement('script');
+//use https when loading script and youtube iframe src since if user is logging in youtube the youtube src will switch to https.
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);    
+
 		</script>
 
 	</body>
