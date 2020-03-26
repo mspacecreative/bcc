ac4072453d11cf3520dcfaad13b2b951bfe91f21
Matt <matt@mspacecreative.com>
2020-03-26 07:27:59 -0300

    c

--- wp-content/themes/bcc/page-home.php
+++ wp-content/themes/bcc/page-home.php
@@ -58,16 +58,16 @@
 							</div>
 							<div class="inner" style="padding-right: 0; padding-left: 0;">
 								<div class="row middle-lg gutter_1">
-								<div class="col-lg-3 col-md-6 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
+								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
 									<a href="/resources#reports"><h3 class="icon-title reports">Reports</h3></a>
 								</div>
-								<div class="col-lg-3 col-md-6 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
+								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
 									<a href="/resources#videos"><h3 class="icon-title videos">Videos</h3></a>
 								</div>
-								<div class="col-lg-3 col-md-6 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
+								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
 									<a href="/resources#relevant-links"><h3 class="icon-title links">Educational</h3></a>
 								</div>
-								<div class="col-lg-3 col-md-6 row col-sm-6 col-xs-12 middle-lg center-lg center-xs">
+								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs">
 									<a href="/resources#photo-gallery"><h3 class="icon-title gallery">Gallery</h3></a>
 								</div>
 							</div>
