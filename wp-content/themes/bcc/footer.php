c9f614c2fe9c8985420d6bcf78caad3decb791d0
Matt <matt@mspacecreative.com>
2020-03-31 19:02:23 -0300

    c

--- wp-content/themes/bcc/footer.php
+++ wp-content/themes/bcc/footer.php
@@ -28,8 +28,8 @@
 			</div>
 			
 		</div><!-- / WRAPPER -->
-
-		<?php wp_footer(); ?>
+		
+		<script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
 		
 		<!-- YOUTUBE TOGGLE SCRIPT -->
 		<script>
@@ -132,6 +132,8 @@
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 		</script>
 		<!-- / YOUTUBE TOGGLE SCRIPT -->
+
+		<?php wp_footer(); ?>
 
 	</body>
 </html>
