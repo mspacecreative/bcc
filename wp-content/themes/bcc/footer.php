a27b4010767607581b1c978c33df852b68dd8e9e
Matt <matt@mspacecreative.com>
2020-03-31 19:10:52 -0300

    c

--- wp-content/themes/bcc/footer.php
+++ wp-content/themes/bcc/footer.php
@@ -29,6 +29,8 @@
 			
 		</div><!-- / WRAPPER -->
 		
+		<script src="https://www.youtube.com/iframe_api"></script>
+		
 		<script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
 		
 		<!-- YOUTUBE TOGGLE SCRIPT -->
