4cd8669fc6998401e59d2a77ee0861731c0e095e
Matt <matt@mspacecreative.com>
2020-04-06 08:33:31 -0300

    c

--- wp-content/themes/bcc/assets/js/main.js
+++ wp-content/themes/bcc/assets/js/main.js
@@ -38,8 +38,8 @@
 		
 		$('.video-youtube').each(function() {
 			$('.video-youtube').click(function(e) {
-				$('.video-youtube').addClass('hide-overlay');
-				$('.video-youtube').children('iframe')[0].src += "&autoplay=1";
+				$(this).addClass('hide-overlay');
+				$(this).children('iframe').src += "&autoplay=1";
 				e.preventDefault();
 			});
 		});
