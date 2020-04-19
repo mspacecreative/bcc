030da2c85f9329e22b4cb10c3de7a43fb3ac9350
Matt <matt@mspacecreative.com>
2020-04-19 13:23:25 -0300

    c

--- wp-content/themes/bcc/single.php
+++ wp-content/themes/bcc/single.php
@@ -1,67 +1,38 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+	<!-- WRAPPER -->
+	<div class="wrapper">
+			
+		<div class="content-wrapper">
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<div class="inner no-bottom-padding">
+				<h1 class="page-title bottom-margin-50"><?php the_title(); ?></h1>
+			</div>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+				<?php the_content(); ?>
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+			</article>
+			<!-- /article -->
 
-			<?php the_content(); // Dynamic Content ?>
+		<?php endwhile; ?>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+		<?php else: ?>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+			<!-- article -->
+			<article>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+			</article>
+			<!-- /article -->
 
-			<?php comments_template(); ?>
+		<?php endif; ?>
 
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+		</div>
 
 <?php get_footer(); ?>
