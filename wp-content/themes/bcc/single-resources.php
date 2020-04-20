3e8cf8df5035be41cd553867ac84360549255c76
Matt <matt@mspacecreative.com>
2020-04-20 15:10:10 -0300

    c

---
+++ wp-content/themes/bcc/single-resources.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+			
+		<div class="content-wrapper">
+
+			<div class="inner no-bottom-padding">
+				<h1 class="page-title bottom-margin-50"><?php the_title(); ?></h1>
+			</div>
+
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php the_content();
+				
+				include 'loops/includes/cta-button-relevant-links-loop.php' ?>
+
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</div>
+
+<?php get_footer(); ?>
