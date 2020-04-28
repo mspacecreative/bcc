b79df8ed9e6e9dae857ebac15330f274fc5fc2bb
Matt <matt@mspacecreative.com>
2020-04-28 17:28:39 -0300

    c

--- wp-content/themes/bcc/functions/functions_shortcodes.php
+++ wp-content/themes/bcc/functions/functions_shortcodes.php
@@ -16,6 +16,30 @@ function relevantLinksLoop() {
 }
 add_shortcode( 'relevant_links_loop', 'relevantLinksLoop' );
 
+// RESOURCE TYPE LINKS LOOP
+function relevantLinksLoop2col() {
+	ob_start();
+		get_template_part('loops/loop-relevant-links-twocol');
+	return ob_get_clean();
+}
+add_shortcode( 'relevant_links_loop_2', 'relevantLinksLoop2col' );
+
+// RESOURCE TYPE LINKS LOOP
+function relevantLinksLoop3col() {
+	ob_start();
+		get_template_part('loops/loop-relevant-links-threecol');
+	return ob_get_clean();
+}
+add_shortcode( 'relevant_links_loop_3', 'relevantLinksLoop3col' );
+
+// RESOURCE TYPE LINKS LOOP
+function relevantLinksLoop4col() {
+	ob_start();
+		get_template_part('loops/loop-relevant-links-fourcol');
+	return ob_get_clean();
+}
+add_shortcode( 'relevant_links_loop_4', 'relevantLinksLoop4col' );
+
 // HOSPITAL FACILITIES MAP
 function hospitalFacilitiesMap() {
 	ob_start();
--- wp-content/themes/bcc/loops/loop-relevant-links-twocol.php
+++ wp-content/themes/bcc/loops/loop-relevant-links-twocol.php
@@ -18,7 +18,7 @@
 			
 	<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 	
-	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">	
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mobile-margin-bottom-25 col-container boxed-link">	
 		
 		<div class="boxed-content">
 			<?php
