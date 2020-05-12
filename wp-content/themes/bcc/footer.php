945f0521a4909c2488d797e0e4972462ebb5ca89
Matt <matt@mspacecreative.com>
2020-05-12 16:48:10 -0300

    c

--- wp-content/themes/bcc/footer.php
+++ wp-content/themes/bcc/footer.php
@@ -1,7 +1,7 @@
 			<footer id="contact" class="section">
 				<div class="inner no-top-bottom-padding light">
 					<div class="row gutter-space-1">
-						<div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 mobile-margin-bottom-25">
+						<div class="col-lg-7 col-md-7 col-sm-6 col-xs-12 mobile-margin-bottom-25 col">
 							<h3>Disclaimer</h3>
 							<p>The Breastfeeding Committee for Canada makes every effort to ensure that other websites directly linked to our own include information compatible with our mission and purpose. We can only be responsible for information contained within our own web pages.</p>
 						</div>
@@ -9,7 +9,7 @@
 							<h3>Subscribe to our newsletter</h3>
 							<?php echo do_shortcode('[contact-form-7 id="585" title="List Builder Form"]'); ?>
 						</div>-->
-						<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+						<div class="col-lg-5 col-md-5 col-sm-6 col-xs-12 col">
 							<h3>Get in touch</h3>
 							<p>
 								<strong>Breastfeeding Committee for Canada</strong><br />
