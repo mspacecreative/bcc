a624bf596b55d6d127fff773a956f536ad81c7b1
Matt <matt@mspacecreative.com>
2020-05-13 09:10:54 -0300

    c

--- wp-content/themes/bcc/page-home.php
+++ wp-content/themes/bcc/page-home.php
@@ -51,25 +51,53 @@
 					<?php the_content(); ?>
 					
 					<!-- RESOURCES -->
-					<div id="resources" class="section resources">
+					<?php 
+					$blockanchor = get_field('block_anchor');
+					$heading = get_field('heading');
+					
+					if ( $blockanchor ): ?>
+					<div id="<?php echo $blockanchor ?>" class="section resources">
+					
+					<?php else : ?>
+					<div class="section resources">
+					<?php endif; ?>
+					
 						<div class="inner">
 							<div class="inner centered-title-with-line-rules padding-bottom-25">
-								<h2><span class="before-title"></span>Resources<span class="after-title"></span></h2>
+								<?php if ( $heading ): ?>
+								<h2><span class="before-title"></span><?php echo $heading ?><span class="after-title"></span></h2>
+								<?php endif; ?>
 							</div>
 							<div class="inner" style="padding-right: 0; padding-left: 0;">
 								<div class="row middle-lg gutter_1 center-lg center-md center-sm center-xs">
+								
+								<?php if ( have_rows('link_column_left') ): while ( have_rows('link_column_left') ): the_row();
+								$link = get_field('link');
+								$label = get_field('label'); ?>
 								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
-									<a href="/resources#videos"><h3 class="icon-title videos">Videos</h3></a>
+									<a href="<?php echo $link ?>"><h3 class="icon-title videos"><?php echo $label ?></h3></a>
 								</div>
+								<?php endwhile;
+								endif; ?>
+								
+								<?php if ( have_rows('link_column_center') ): while ( have_rows('link_column_center') ): the_row();
+								$link = get_field('link');
+								$label = get_field('label'); ?>
 								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
-									<a href="/resources#relevant-links"><h3 class="icon-title links">Educational</h3></a>
+									<a href="<?php echo $link ?>"><h3 class="icon-title links"><?php echo $label ?></h3></a>
 								</div>
-								<!--<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs">
-									<a href="/resources#photo-gallery"><h3 class="icon-title gallery">Gallery</h3></a>
-								</div>-->
+								<?php endwhile;
+								endif; ?>
+								
+								<?php if ( have_rows('link_column_right') ): while ( have_rows('link_column_right') ): the_row();
+								$link = get_field('link');
+								$label = get_field('label'); ?>
 								<div class="col-lg-3 col-md-3 row col-sm-6 col-xs-12 middle-lg center-lg center-xs mobile-margin-bottom-50">
-									<a href="/resources#reports"><h3 class="icon-title reports">Reports</h3></a>
+									<a href="<?php echo $link ?>"><h3 class="icon-title reports"><?php echo $label ?></h3></a>
 								</div>
+								<?php endwhile;
+								endif; ?>
+								
 							</div>
 							</div>
 						</div>
