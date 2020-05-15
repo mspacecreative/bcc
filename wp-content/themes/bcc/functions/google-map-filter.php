6afeb0b131b98e1c61082aec9e3082bd5e84c370
Matt <matt@mspacecreative.com>
2020-05-15 09:36:46 -0300

    c

--- wp-content/themes/bcc/functions/google-map-filter.php
+++ wp-content/themes/bcc/functions/google-map-filter.php
@@ -11,3 +11,11 @@ function my_acf_google_map_api( $api ){
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+// CHANGE MAP TO FRENCH
+add_action( 'wp_enqueue_scripts', function() {
+	$language = 'fr-FR';
+
+	wp_deregister_script( 'aa_js_googlemaps_script' );
+	wp_enqueue_script( 'aa_js_googlemaps_script_fre', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs' . c27()->get_setting( 'general_google_maps_api_key' ) . '&libraries=places&v=3&language=' . $language, [], null, true );
+}, 100 );
