7f182e0a817ec5fc81a765a9a5f35439c5065c4f
Matt <matt@mspacecreative.com>
2020-05-15 09:47:45 -0300

    c

--- wp-content/themes/bcc/functions/google-map-filter.php
+++ wp-content/themes/bcc/functions/google-map-filter.php
@@ -13,10 +13,13 @@ function my_acf_google_map_api( $api ){
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 // CHANGE MAP TO FRENCH
-add_action( 'wp_enqueue_scripts', function() {
-	$language = 'fr-FR';
 
-	wp_deregister_script( 'aa_js_googlemaps_script' );
-	wp_register_script( 'aa_js_googlemaps_script_fre', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs' . '&language=' . $language, array('jquery'), null);
-	wp_enqueue_script('aa_js_googlemaps_script_fre');
-}, 100 );
+if (pll_current_language() == 'fr-FR') {
+	add_action( 'wp_enqueue_scripts', function() {
+		$language = 'fr-FR';
+	
+		wp_deregister_script( 'aa_js_googlemaps_script' );
+		wp_register_script( 'aa_js_googlemaps_script_fre', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs' . '&language=' . $language, array('jquery'), null);
+		wp_enqueue_script('aa_js_googlemaps_script_fre');
+	}, 100 );
+}
