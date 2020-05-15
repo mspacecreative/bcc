ad2fcf91854ef08721e290aafd09f10db720d10b
Matt <matt@mspacecreative.com>
2020-05-15 09:21:28 -0300

    c

--- wp-content/themes/bcc/functions/google-map-filter.php
+++ wp-content/themes/bcc/functions/google-map-filter.php
@@ -1,8 +1,15 @@
 <?php
 function googleMapAPIScript() {
-	wp_register_script('aa_js_googlemaps_script',  'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs', array('jquery'), null);
-
-	wp_enqueue_script('aa_js_googlemaps_script');
+	
+	if (pll_current_language() == 'en-CA') {
+	
+		wp_register_script('aa_js_googlemaps_script',  'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs', array('jquery'), null);
+		wp_enqueue_script('aa_js_googlemaps_script');
+	
+	} elseif (pll_current_language() == 'fr-FR') {
+		wp_register_script('aa_js_googlemaps_script',  'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs&language=fr', array('jquery'), null);
+		wp_enqueue_script('aa_js_googlemaps_script');
+	}
 }
 add_action('wp_enqueue_scripts', 'googleMapAPIScript');
 
