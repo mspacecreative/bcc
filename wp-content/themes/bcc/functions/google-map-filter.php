072a828d209b15762683a6709cd44f8605a6685d
Matt <matt@mspacecreative.com>
2020-05-15 09:45:46 -0300

    c

--- wp-content/themes/bcc/functions/google-map-filter.php
+++ wp-content/themes/bcc/functions/google-map-filter.php
@@ -1,6 +1,6 @@
 <?php
 function googleMapAPIScript() {
-	wp_register_script('aa_js_googlemaps_script',  'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs', array('jquery'), null);
+	wp_register_script('aa_js_googlemaps_script', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs', array('jquery'), null);
 
 	wp_enqueue_script('aa_js_googlemaps_script');
 }
@@ -17,5 +17,6 @@ add_action( 'wp_enqueue_scripts', function() {
 	$language = 'fr-FR';
 
 	wp_deregister_script( 'aa_js_googlemaps_script' );
-	wp_enqueue_script( 'c27-google-maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=' . c27()->get_setting( 'AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs' ) . '&libraries=places&v=3&language=' . $language, [], null, true );
+	wp_register_script( 'aa_js_googlemaps_script_fre', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyA7bgPEv6D1DOFBlFH9JSnnsC-2VV4xYrs' . '&language=' . $language, array('jquery'), null);
+	wp_enqueue_script('aa_js_googlemaps_script_fre');
 }, 100 );
