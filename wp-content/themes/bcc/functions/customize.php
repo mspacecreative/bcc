9c1bc24706f6f646fee6dea6fd7b9d7b31878530
Matt <matt@mspacecreative.com>
2020-06-03 09:04:33 -0300

    c

--- wp-content/themes/bcc/functions/customize.php
+++ wp-content/themes/bcc/functions/customize.php
@@ -1,15 +1,13 @@
 <?php 
-/**
- * Remove Admin Menu Link to Theme Customizer
- */
 add_action( 'admin_menu', function () {
-    global $submenu;
-
-    if ( isset( $submenu[ 'themes.php' ] ) ) {
-        foreach ( $submenu[ 'themes.php' ] as $index => $menu_item ) {
-            if ( in_array( array( 'Customize', 'Customizer', 'customize' ), $menu_item ) ) {
+global $submenu;
+if ( isset( $submenu[ 'themes.php' ] ) ) {
+    foreach ( $submenu[ 'themes.php' ] as $index => $menu_item ) {
+        foreach ($menu_item as $value) {
+            if (strpos($value,'customize') !== false) {
                 unset( $submenu[ 'themes.php' ][ $index ] );
             }
         }
     }
+}
 });
